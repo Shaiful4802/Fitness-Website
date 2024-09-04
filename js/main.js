5ed97335d59419fb894d350a1bd7c1bd8e557229
Shaiful4802 <office.shaiful@gmail.com>
2024-09-04 12:32:27 +0600

    update sticky menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // all jquery start
+
 $(document).ready(function($){
+
+    //  Counter Function start
     $('.counter').counterUp({
         delay: 10,
         time: 1000,
@@ -9,5 +12,19 @@ $(document).ready(function($){
           return n.replace(/,/g, '.');
         }
     });
-})
 
+
+    // sticky Menu fundtion
+    let nav = document.querySelector("#header");
+
+    window.addEventListener("scroll", function(){
+        let sticky = window.scrollY;
+        if ( sticky > 100){
+            nav.classList.add("sticky-menu")
+        } 
+        else {
+            nav.classList.remove("sticky-menu")
+        }
+    })
+
+})
